dcd7274bf925e6ff3e7f9b0d0c451c850cf08c6a
manhdev123 <ducmanh1928z@.com>
2026-01-02 19:37:14 +0700

    test

--- src/utilities/Validation.java
+++ src/utilities/Validation.java
@@ -2,7 +2,7 @@ package utilities;
 
 public class Validation {
     public static void main(String[] args) {
-        System.err.println("hello");
+        System.err.println("hello"); 
     }
     
 }
